5514f886585e50a599c58e821c089828260ad33e
stig <stig@b3023d8d-9cf6-0310-973e-fe5e02187dc7>
2007-09-01 18:47:16 +0000

    profiling script more useful

--- ReversiProfile.m
+++ ReversiProfile.m
@@ -5,22 +5,46 @@
 int main(int argc, char **argv)
 {
     NSAutoreleasePool *pool = [NSAutoreleasePool new];
+    NSMutableDictionary *opts = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+        @"0",           @"--mutable",
+        @"1",           @"--count",
+        @"0",           @"--skip",
+        @"5",           @"--ply",
+        [NSNull null],  @"--time",
+        nil];
 
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s cnt ply [mutable]\n", argv[0]);
+    if (argc < 3 || !(argc % 2)) {
+        fprintf(stderr, "Usage: %s [--opt value]\n", argv[0]);
         return EXIT_FAILURE;
     }
-    int cnt = atoi(argv[1]);
-    int ply = atoi(argv[2]);
-    int mutable = argc > 3 && atoi(argv[3]);
-    Class class = mutable ? [SBMutableReversiState class]: [SBReversiState class];
-        
+    for (int i = 1; i < argc; i += 2)
+        [opts setObject:[NSString stringWithCString:argv[i+1]]
+                 forKey:[NSString stringWithCString:argv[i]]];
+
+    Class class = [[opts objectForKey:@"--mutable"] intValue]
+        ? [SBMutableReversiState class]
+        : [SBReversiState class];
     SBAlphaBeta *ab = [SBAlphaBeta newWithState:[class new]];
-    for (int i = 0; i < cnt; i++) {
+
+    int skip = [[opts objectForKey:@"--skip"] intValue];
+    for (int i = 0; i < skip; i++)
+        [ab applyMove:[[ab movesAvailable] objectAtIndex:0]];
+
+    int fixed = [[opts objectForKey:@"--time"] isKindOfClass:[NSNull class]];
+    int ply = [[opts objectForKey:@"--ply"] intValue];
+    for (int i = 0; i < [[opts objectForKey:@"--count"] intValue]; i++) {
         NSDate *date = [NSDate date];
-        [ab moveFromSearchWithPly:ply];
-        printf("%lf\n", (double)-[date timeIntervalSinceNow]);
-        date = [NSDate date];
+        if (fixed)
+            [ab moveFromSearchWithPly:ply];
+        else
+            [ab moveFromSearchWithInterval:[[opts objectForKey:@"--time"] doubleValue]];
+
+        printf("%u %lf %u %u (visited/time/ply/skip)\n",
+            [ab countStatesVisited],
+            (double)-[date timeIntervalSinceNow],
+            fixed ? ply : [ab plyReachedForSearch],
+            skip
+        );
     }
     
     [pool release];
