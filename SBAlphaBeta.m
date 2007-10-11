5b3dcf6d9962ba08a03499b8dc5bf56b7f17c516
stig <stig@b3023d8d-9cf6-0310-973e-fe5e02187dc7>
2007-10-11 10:32:17 +0000

    kill memory leak

--- SBAlphaBeta.m
+++ SBAlphaBeta.m
@@ -72,7 +72,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
         [state applyMove:m];
     
     } else {
-        state = [state copy];
+        state = [[state copy] autorelease];
         [state applyMove:m];
         [stateHistory addObject:state];
     }
